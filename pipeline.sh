6446753f78e0e586e673fff18a2a3fa521c2f340
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2017-07-11 16:34:16 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -6,3 +6,5 @@ runc 'oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-
 runc 'oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n qa'
 runc 'oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n qa'
 runc 'oc policy add-role-to-group system:image-puller system:serviceaccounts:qa -n dev'
+runc 'oc create -f https://raw.githubusercontent.com/debianmaster/istio-openshift-example/master/pipeline.yaml'
+
